75c2975b64ce9c603e27d954d2646bc931deb82c
SenseiQt07 <86264964+SenseiQt07@users.noreply.github.com>
2021-06-22 03:38:56 +0530

    Update Webhook.py

--- Webhook.py
+++ Webhook.py
@@ -1,15 +1,15 @@
-import requests        #requirement if you don't have this module just open cmd and type (pip install requests)
+import requests
 
 
 
 
 def main():     
-    url = input("Enter Webhook Url : ")           #IMPORTANT!!!  For example = https://discord.com/api/webhooks/webhook_id/webhook_token
-    username = input("Enter Webhook username : ")   #The name to display on current message (you can leave this blank if you want)
-    picture = input("Enter Webhook Avatar Url : ")  #Update avatar on current webhook message(you can leave this blank as well)
-    message = input("Enter the message you want to send : ") #INPORTANT!!!
-    data = {"content":message,"username":username,"avatar_url":picture}   #main data to be stored 
-    r = requests.post(url,data=data)
+    url = input("Enter Webhook Url : ")                                   #For example = https://discord.com/api/webhooks/webhook_id/webhook_token
+    username = input("Enter Webhook username : ")                         #The name to display on current message (you can leave this blank if you want)
+    picture = input("Enter Webhook Avatar Url : ")                        #Update avatar on current webhook message(you can leave this blank as well)
+    message = input("Enter the message you want to send : ")              
+    data = {"content":message,"username":username,"avatar_url":picture}   #main data to be stored as variable
+    r = requests.post(url,data=data) #sending the message 
     if r.status_code == 204:     #Status Code shown when request was completed.
         print("Message Sent Successfully !")
     else :
