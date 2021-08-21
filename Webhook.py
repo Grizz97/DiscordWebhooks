4e953b692e7ce310b40b3ac35363f40e219bcd21
Aryaman Singh <86264964+SenseiQt07@users.noreply.github.com>
2021-08-21 23:53:18 +0530

    Update Webhook.py

--- Webhook.py
+++ Webhook.py
@@ -4,13 +4,13 @@ import requests
 
 
 def main():     
-    url = input("Enter Webhook Url : ")                                   #For example = https://discord.com/api/webhooks/webhook_id/webhook_token
-    username = input("Enter Webhook username : ")                         #The name to display on current message (you can leave this blank if you want)
-    picture = input("Enter Webhook Avatar Url : ")                        #Update avatar on current webhook message(you can leave this blank as well)
+    url = input("Enter Webhook Url : ")                                  
+    username = input("Enter Webhook username : ")                      
+    picture = input("Enter Webhook Avatar Url : ")                   
     message = input("Enter the message you want to send : ")              
-    data = {"content":message,"username":username,"avatar_url":picture}   #main data to be stored as variable
-    r = requests.post(url,data=data)                                      #sending the message 
-    if r.status_code == 204:                                              #Status Code shown when request was completed.
+    data = {"content":message,"username":username,"avatar_url":picture}   
+    r = requests.post(url,data=data)                                 
+    if r.status_code == 204:                                        
         print("Message Sent Successfully !")
     else :
         print("Message Not Sent")
